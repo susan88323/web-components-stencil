7f653ac11f0454a24e0014fde5effe00d283ee6b
Susan Wang <susanw@dev6.com>
2019-11-26 22:07:45 -0500

    Created the first component

---
+++ src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,14 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+    tag: 'uc-side-drawer'
+})
+export class SideDrawer {
+    render() {
+        return (
+            <div>
+                <h1>The side drawer</h1>
+            </div>
+        );
+    }
+}
