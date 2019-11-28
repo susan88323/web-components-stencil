6f3d5c6554926ce7b58a82eb80f6bc51afec37ae
Susan Wang <susanw@dev6.com>
2019-11-28 11:51:33 -0500

    Use attributes to style the side drawer

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -8,8 +8,20 @@ import { Component, Prop, h } from '@stencil/core';
 export class SideDrawer {
     // with reflect true, the attribute will be updated whenever the prop changes
     @Prop({reflect: true}) title: string;
+    // @Prop() open: boolean;
 
     render() {
+        // let content = null;
+        // if (this.open) {
+        //     content = (
+        //         <aside>
+        //             <header><h1>{this.title}</h1></header>
+        //             <main>
+        //                 <slot />
+        //             </main>
+        //         </aside>
+        //     );
+        // }
         return (
             <aside>
                 <header><h1>{this.title}</h1></header>
