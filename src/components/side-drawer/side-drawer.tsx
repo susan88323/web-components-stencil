ea3977c520898717a832b12cfa86fe133545a51a
Susan Wang <susanw@dev6.com>
2019-11-28 22:26:52 -0500

    Added button click event listener

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, Prop, h, State } from '@stencil/core';
 
 @Component({
     tag: 'uc-side-drawer',
@@ -6,6 +6,8 @@ import { Component, Prop, h } from '@stencil/core';
     shadow: true
 })
 export class SideDrawer {
+    @State() showContactInfo = false;
+
     // with reflect true, the attribute will be updated whenever the prop changes
     @Prop({reflect: true}) title: string;
     @Prop({reflect: true, mutable: true}) open: boolean;
@@ -14,7 +16,29 @@ export class SideDrawer {
         this.open = false;
     }
 
+    onContentChange(content: string) {
+        this.showContactInfo = content === 'contact';
+    }
+
     render() {
+        let mainContent = <slot />;
+        if (this.showContactInfo) {
+            mainContent = (
+                <div id="contact-information">
+                    <h2>Contact Information</h2>
+                    <p>You can reach us via phone or email.</p>
+                    <ul>
+                        <li>
+                            Phone: 3883939992
+                        </li>
+                        <li>
+                            E-mail: <a href="mailto: test@test.com">test@test.com</a>
+                        </li>
+                    </ul>
+                </div>
+            )
+        }
+        
         // let content = null;
         // if (this.open) {
         //     content = (
@@ -32,8 +56,12 @@ export class SideDrawer {
                     <h1>{this.title}</h1>
                     <button onClick={this.onCloseDrawer.bind(this)}>X</button>
                 </header>
+                <section id="tabs">
+                    <button class={!this.showContactInfo ? 'active' : ''} onClick={this.onContentChange.bind(this, 'nav')}>Navigation</button>
+                    <button class={this.showContactInfo ? 'active' : ''} onClick={this.onContentChange.bind(this, 'contact')}>Contact</button>
+                </section>
                 <main>
-                    <slot />
+                    {mainContent}
                 </main>
             </aside>
         );
