e8c1145e9976e1f15568535eaf61fac692b6ca5e
Susan Wang <susanw@dev6.com>
2019-11-29 22:12:03 -0500

    Added error handling

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -13,6 +13,7 @@ export class StockPrice {
     @State() fetchedPrice: number;
     @State() stockUserInput: string; // Set two way binding
     @State() stockInputValid = false;
+    @State() error: string;
 
     onUserInput(event: Event) {
         this.stockUserInput = (event.target as HTMLInputElement).value;
@@ -31,17 +32,31 @@ export class StockPrice {
         const stockSymbol = this.stockInput.value;
         fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`)
             .then(res => {
+                if (res.status !== 200) {
+                    throw new Error('Invalid');
+                }
                 return res.json();
             })
             .then(parsedRes => {
+                if (!parsedRes['Global Quote']['05. price']) {
+                    throw new Error('Invalid Symbol');
+                }
+                this.error = null;
                 this.fetchedPrice = +parsedRes['Global Quote']['05. price'];
             })
             .catch(err => {
-                console.log(err);
+                this.error = err.message;
             });
     }
 
     render() {
+        let dataContent = <p>Please enter a symbol!</p>;
+        if (this.error) {
+            dataContent = <p>{this.error}</p>
+        }
+        if (this.fetchedPrice) {
+            dataContent = <p>Price: ${this.fetchedPrice}</p>;
+        }
         return [
             <form onSubmit={this.onFetchStockPrice.bind(this)}>
                 <input
@@ -52,7 +67,7 @@ export class StockPrice {
                 <button type="submit" disabled={!this.stockInputValid}>Fetch</button>
             </form>,
             <div>
-                <p>Price: ${this.fetchedPrice}</p>
+                {dataContent}
             </div>
         ];
     }
