95faabd2460af8b8ee683866896f1dfa25932112
Susan Wang <susanw@dev6.com>
2019-11-30 22:29:24 -0500

    Added a list of elements

--- src/components/stock-finder/stock-finder.tsx
+++ src/components/stock-finder/stock-finder.tsx
@@ -10,13 +10,17 @@ import { AV_API_KEY } from '../../global/global';
 export class StockFinder {
     stockNameInput: HTMLInputElement;
 
+    @State() searchResults: {symbol: string, name: string}[] = [];
+
     onFindStocks(event: Event) {
         event.preventDefault();
         const stockName = this.stockNameInput.value;
         fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stockName}&apikey=${AV_API_KEY}`)
             .then(res => res.json())
             .then(parsedRes => {
-                console.log(parsedRes);
+                this.searchResults = parsedRes['bestMatches'].map(match => {
+                    return {name: match['2. name'], symbol: match['1. symbol']};
+                })
             }).catch(err => console.log(err));
     }
 
@@ -28,6 +32,11 @@ export class StockFinder {
                     ref={el => this.stockNameInput = el} />
                 <button type="submit">Find!</button>
             </form>,
+            <ul>
+                {this.searchResults.map(result => (
+                    <li><strong>{result.symbol}</strong> - {result.name}</li>
+                ))}
+            </ul>
         ];
     }
 }
