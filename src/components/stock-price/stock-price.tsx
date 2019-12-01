0e74e75e56cb7fe6c044ec9b6e03684e547ab548
Susan Wang <susanw@dev6.com>
2019-12-01 10:55:39 -0500

    Added listen decorator to listen to the custom event

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component, h, State, Element, Prop, Watch } from '@stencil/core';
+import { Component, h, State, Element, Prop, Watch, Listen } from '@stencil/core';
 
 import { AV_API_KEY } from '../../global/global';
 
@@ -23,6 +23,7 @@ export class StockPrice {
     stockSymbolChanged(newValue: string, oldValue: string) {
         if (newValue !== oldValue) {
             this.stockUserInput = newValue;
+            this.stockInputValid = true;
             this.fetchStockPrice(newValue);
         }
     }
@@ -74,6 +75,14 @@ export class StockPrice {
 
     componentDidUnload() {
         console.log('componentDidUnload');
+    }
+
+    @Listen('body:ucSymbolSelected')
+    onStockSymbolSelected(event: CustomEvent) {
+        console.log('stock symbol selected: ' + event.detail);
+        if (event.detail && event.detail !== this.stockSymbol) {
+            this.stockSymbol = event.detail;
+        }
     }
 
     fetchStockPrice(stockSymbol) {
